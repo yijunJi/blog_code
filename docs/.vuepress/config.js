7dbce677d33ad380b68411cacbd13a7b1f4dda2e
jiyijun <jiyijun@maoyan.com>
2021-09-22 19:38:17 +0800

    feat: blog start

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Web 前端',
+  description: 'jiyijun.github.io',
+  plugins: ['@vuepress/back-to-top'],
+  themeConfig: {
+    displayAllHeaders: true,
+    nav: [
+      { text: 'Github', link: 'https://github.com/yijunJi?tab=repositories' },
+    ],
+    // sidebar: [
+    //   {
+    //     title: '指南',
+    //     path: '/',
+    //     collapsable: false,
+    //   }
+    // ]
+  }
+}
