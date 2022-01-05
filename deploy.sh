60df13de97fc546e8c8159c574e28fe67a86b92b
jiyijun <jiyijun@maoyan.com>
2022-01-05 16:21:18 +0800

    feat: 自动化打包无法使用，兼容方案

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!usr/bin/env sh
 #当发生错误中止脚本
 set -e
+# 打包
+yarn build
 # cd 到构建输出的目录下
 cd ./docs/.vuepress/dist
+# 打包后的文件上传到github主页
 git init
 git add -A
 git commit -m 'deploy'
-# 这里的access_token是travis设置的环境变量
-git push -f https://${jiyijun}@github.com/yijunJi/yijunJi.github.io.git master
-cd -  # 返回上一层
+git remote add origin git@github.com:yijunJi/yijunJi.github.io.git
+git push -f origin master
+rm -rf ../dist
+cd -
